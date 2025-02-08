c6ba16081612174a26b524ca405ea82c80278b24
Anokye James Yaw <jamesyawanokye17@gmail.com>
2025-02-08 14:59:39 +0000

    Deploy

--- cardano-cart-frontend/next.config.mjs
+++ cardano-cart-frontend/next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      domains: ['localhost', 'charming-ninnetta-knust-028ea081.koyeb.app'],
-    },
+    domains: ['localhost', 'charming-ninnetta-knust-028ea081.koyeb.app'],
+  },
+  webpack: (config) => {
+    config.experiments = { asyncWebAssembly: true, topLevelAwait: true };
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "webassembly/async",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
